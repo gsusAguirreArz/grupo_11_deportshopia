f7cd2487127357b74853c1f1085aa5639907e01e
carlostnorio <42305499+jocarote@users.noreply.github.com>
2021-06-22 16:49:40 -0500

    Se agregó el esqueleto del login y del registro

--- site_source/app.js
+++ site_source/app.js
@@ -1,40 +1,34 @@
 const express = require('express');
+const app = express();
 var path = require('path');
-
-var porductsController = require("./controllers/products")
-var product = require("./models/product")
-
-var app = express();
-
-const PORT = process.env.PORT || 8080;
-
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log('Server running on http://localhost:'+PORT);
+app.listen(3000, () => {
+    console.log('Servidor funcionando');
 });
 
+
 app.get('/', (req,res) => {
-    let file = path.join(__dirname, './views/index.html');
-    res.sendFile(file);
+    let file1 = path.join(__dirname, './views/index.html');
+    res.sendFile(file1);
 });
 
 app.get('/product', (req,res) => {
-    let file = path.join(__dirname, './views/productDetail.html');
-    res.sendFile(file);
+    let file2 = path.join(__dirname, './views/productDetail.html');
+    res.sendFile(file2);
 });
 
 app.get('/login', (req,res) => {
-    let file = path.join(__dirname, './views/login.html');
-    res.sendFile(file);
+    let file3 = path.join(__dirname, './views/login.html');
+    res.sendFile(file3);
 });
 
 app.get('/cart', (req,res) => {
-    let file = path.join(__dirname, './views/productCart.html');
-    res.sendFile(file);
+    let file4 = path.join(__dirname, './views/productCart.html');
+    res.sendFile(file4);
 });
 
 app.get('/register', (req,res) => {
-    let file = path.join(__dirname, './views/register.html');
-    res.sendFile(file);
+    let file5 = path.join(__dirname, './views/register.html');
+    res.sendFile(file5);
 });
