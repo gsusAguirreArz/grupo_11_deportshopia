352c8f51ab6293693572797f12e496b309bb177e
gsusAguirreArz <daniel.aguirre27@outlook.com>
2021-10-21 02:09:49 -0500

    updates

--- site/backend/src/routes/adminDash.js
+++ site/backend/src/routes/adminDash.js
@@ -1,6 +1,8 @@
 // ------------------- Import's -------------------
 const express = require('express');
 const adminDashController = require('../controllers/adminDash');
+const userNotLogged = require('../middlewares/routes/auth/userNotLogged');
+const userAdmin = require('../middlewares/routes/auth/userAdmin');
 
 // **** Router ****
 const router = express.Router();
@@ -8,7 +10,7 @@ const router = express.Router();
 // **** Routes ****
 
 // Show all users
-router.get('/', adminDashController.index);
+router.get('/', [userNotLogged,userAdmin], adminDashController.index);
 
 
 // ------------------- Exports -------------------
